e55f40054f26b7b0f84fcc9a2d9cb7d9be659445
Mark Antao <35659231+markantao@users.noreply.github.com>
2021-11-16 13:56:28 -0500

    error with user key presses

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 import curses
 from curses import wrapper
 
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr(0, 0, "Speed Typing Test")
+    stdscr.addstr("\nBy: Mark Antao")
+    stdscr.refresh()
+    stdscr.getkey()
+
+def wpm_test(stdscr):
+    target_text = "Hello World, this is an example text!"
+    current_text = []
+    stdscr.clear()
+    stdscr.addstr(target_text)
+    stdscr.refresh()
+
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    stdscr.clear()
-    stdscr.addstr(1, 5, "Test")
-    stdscr.refresh()
-    stdscr.getkey()
+
+    start_screen(stdscr)
+    wpm_test(stdscr)
+
 
 wrapper(main)
