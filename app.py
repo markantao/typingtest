f640a6e4e1b7d1d7610eef8f2cb13f1498df76ba
Mark Antao <35659231+markantao@users.noreply.github.com>
2021-11-19 18:41:15 -0500

    completed wpm functionality

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Imports
 import curses
 from curses import wrapper
+import time
 
 #Starting Screen
 def start_screen(stdscr):
@@ -31,16 +32,34 @@ def wpm_test(stdscr):
     target_text = "Hello World, this is an example text!"
     current_text = []
     wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True) # Do not wait for the user to click a key (for wpm)
 
 
 #Typing Characters
     while True:
+        time_elapsed = max(time.time() - start_time, 1) #calculating time
+        # We use "max" and "1" so that we don't get a division by 1 error
+        # Explain division by 1 error in readme.md
+        wpm = round((len(current_text) / (time_elapsed / 60)) / 5) # The average word is 5 characters
+        # wpm "algorithm" above.
+        # We round the "algorithm" so that we don't get a decimal number but rather a whole number
+        #explain "algorith" in readme.md
+
+# Explain all the things that I learnt in the readme.md such as division by 1 error and the "algorith" for wpm and such
 
         stdscr.clear() # Clears terminal
         display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh() # Refresh terminal to show changes
 
-        key = stdscr.getkey()
+        #Making sure that line 57 won't crash, if it does, the continue keyword will bring us back 
+        # to the start of the while loop and it skips from line 66 as you cannot check what key was
+        # was pressed if no key was pressed. (Has no value)
+
+        try:
+            key = stdscr.getkey() # This is known as a block; kind of like an input
+        except:
+            continue
 
         # The ordinal (ord) value of a key is its numerical value on the keyboard
         #ASCII 27 == Escape Key
